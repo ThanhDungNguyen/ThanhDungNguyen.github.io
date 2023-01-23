331a9be2fa9fc85239b58eb727a2a136a0c0e5f3
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2023-01-23 08:46:10 -0600

    Implement animation

--- scripts/w1d5_ascii.js
+++ scripts/w1d5_ascii.js
@@ -2,6 +2,8 @@ let startButton;
 let stopButton;
 let animationSelector;
 let textArea;
+let animationArray;
+let interval;
 
 window.onload = function() {
     textArea = document.getElementById("text-area");
@@ -17,14 +19,26 @@ window.onload = function() {
 function startAnimation() {
     startButton.disabled = true;
     stopButton.disabled = false;
+
+    animationArray = textArea.value.split("=====\n");
+    interval = setInterval(updateAnimation, 250);
 }
 
 function stopAnimation() {
     startButton.disabled = false;
     stopButton.disabled = true;
+
+    clearInterval(interval);
+    loadAnimation();
 }
 
 function loadAnimation() {
     let animationName = animationSelector.value;
     textArea.value = ANIMATIONS[animationName];
+}
+
+function updateAnimation() {
+    let firstAnimation = animationArray.shift();
+    textArea.value = firstAnimation;
+    animationArray.push(firstAnimation);
 }
