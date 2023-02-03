064232402706f34bcc525a35064c89356025ac0a
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2023-02-03 08:46:52 -0600

    Send HTML page

--- w3d4/q2/index.js
+++ w3d4/q2/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(`Hello`);
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(3000);
