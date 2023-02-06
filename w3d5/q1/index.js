498b133fc98216d7a9ed85ddcd3559c992be78a4
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2023-02-05 20:29:30 -0600

    W3D5 Question 1 - Load css file base on the hour of the day

--- w3d5/q1/index.js
+++ w3d5/q1/index.js
@@ -5,10 +5,16 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "view"));
 
+app.use('/styles', express.static(path.join(__dirname, 'styles')));
+
 app.get('/', (req, res) => {
     const date = new Date();
+
+    let cssFilename = (date.getHours() > 6 && date.getHours() < 18) ? "day.css" : "night.css";
+
     res.render("index", {
         time: date.toTimeString(),
+        cssFilename: cssFilename
     });
 });
 
