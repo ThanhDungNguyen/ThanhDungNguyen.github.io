3655f59a6fe0fec95e99e7f4df56a4f74922cb61
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2023-02-06 20:04:58 -0600

    W4D1 - Submit form value

--- w4d1/q1/app.js
+++ w4d1/q1/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.render("add-cookie");
+});
+
+app.post("/add", (req, res) => {
+    console.log(req.body);
+    res.send("Cookie has been set");
 });
 
 app.listen(3000, () => {
