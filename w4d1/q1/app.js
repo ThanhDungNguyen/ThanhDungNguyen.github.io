4c3c22b21a999fc7791d5dbbe4f0aaf707d318d4
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2023-02-06 18:38:33 -0600

    W4D1 - Add form

---
+++ w4d1/q1/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("add-cookie");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running...");
+});
