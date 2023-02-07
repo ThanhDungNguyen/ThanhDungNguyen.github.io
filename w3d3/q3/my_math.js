2e587698e0dd21bfc9e945a247da201ff37e2a69
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2023-02-07 17:23:54 -0600

    W3D3 - Q3 - Export functions

---
+++ w3d3/q3/my_math.js
@@ -0,0 +1,21 @@
+class Math {
+    add(x, y) {
+        return x + y;
+    }
+
+    subtract(x, y) {
+        return x - y;
+    }
+
+    multiply(x, y) {
+        return x * y;
+    }
+
+    divide(x, y) {
+        return x / y;
+    }
+}
+
+let math = new Math();
+
+module.exports = math;
