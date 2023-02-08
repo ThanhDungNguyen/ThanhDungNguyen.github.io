8b88c027ee49afbb498701cdb747cedce3e43fa1
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2023-02-07 20:57:41 -0600

    W4D2 - Q2 - Send question to server

--- w4d2/q2/app.js
+++ w4d2/q2/app.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    console.log("Received GET request");
+    console.log("Received GET request: " + req.query.question);
     res.render("index");
 });
 
--- w4d2/q2/public/ask8ball.js
+++ w4d2/q2/public/ask8ball.js
@@ -4,6 +4,19 @@ $(function(){
 });
 
 function submit() {
-    let question = $("#question").val();
-    console.log("Question: " + question);
+    let questionValue = $("#question").val();
+    console.log("Question: " + questionValue);
+    $.ajax({
+        type: "GET",
+        url: "/",
+        data: { question: questionValue}
+    }).done(successHandler).fail(failHandler);
+}
+
+function successHandler(data) {
+    console.log("Success Handler: " + data);
+}
+
+function failHandler(jqXHR, status, error) {
+    console.log("Error: " + error);
 }
